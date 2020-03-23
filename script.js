02f02154910ecc14ca66c09cca9f4f0f8372999d
daniprol <daniprol@hotmail.com>
2020-03-23 21:37:40 +0100

    Timer and endgame added

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const word = document.getElementById("word");
 const text = document.getElementById("text");
 const scoreEl = document.getElementById("score");
 const timeEl = document.getElementById("time");
-const endgameEl = document.getElementById("eng-game");
+const endgameEl = document.getElementById("end-game-container");
 const settingsBtn = document.getElementById("settings-btn");
 const settings = document.getElementById("settings");
 const settingsForm = document.getElementById("settings-form");
@@ -51,6 +51,12 @@ let score = 0;
 // Init time:
 let time = 10;
 
+// Focus on text on start:
+text.focus();
+
+// Start counting down:
+const timeInterval = setInterval(updateTime, 1000);
+
 // Generate random word from array
 function getRandomWord() {
   return words[Math.floor(Math.random() * words.length)];
@@ -69,6 +75,29 @@ function updateScore() {
   score++;
   scoreEl.innerHTML = score;
 }
+
+// Update time:
+function updateTime() {
+  // console.log(1); // It logs every second
+  time--;
+  timeEl.innerHTML = time + "s";
+
+  if (time === 0) {
+    clearInterval(timeInterval);
+    // End game
+    gameOver();
+  }
+}
+
+// Game over, show end screen
+function gameOver() {
+  endgameEl.innerHTML = `
+    <h1>Time ran out</h1>
+    <p>Your final score is ${score}</p>
+    <button onclick="location.reload()">Reload</button>
+    `;
+  endgameEl.style.display = "flex";
+}
 addWordToDOM();
 
 // Event listeners:
@@ -80,5 +109,8 @@ text.addEventListener("input", e => {
     updateScore();
     // clear input:
     e.target.value = "";
+
+    time += 5;
+    updateTime();
   }
 });
